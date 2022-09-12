4c7ea03fbb6fb487a16fe6d8e142ab29774b3d3b
shunya9811 <11wash27@gmail.com>
2022-09-12 18:35:34 +0900

    material ui

--- src/App.js
+++ src/App.js
@@ -3,18 +3,43 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Error from "./Pages/Error";
+
+
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
 const homeUrl = process.env.PUBLIC_URL;
 
 function App() {
 　　return (
-　　　　<Router>
-　　　　　　<Routes>
-　　　　　　　　<Route path={ homeUrl } element={<Home />} />
-　　　　　　　　<Route path={ homeUrl + "/about" } element={<About />} />
-　　　　　　　　<Route path="*" element={<Error />} />
-　　　　　　</Routes>
-　　　　</Router>
-　　);
+      <div className='App'>
+        <header className='center right'>
+            <h1 className='title'>Electric Commerce</h1>
+          <div className='headerBtn'>
+            <IconButton>
+                <ShoppingCartIcon
+                  fontSize='large'
+                />
+            </IconButton>
+            <IconButton>
+                <FavoriteIcon 
+                  fontSize='large'
+                />
+            </IconButton>
+          </div>
+        </header>
+        <main>
+          <Router>
+            <Routes>
+              <Route path={ homeUrl } element={<Home />} />
+              <Route path={ homeUrl + "/about" } element={<About />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </Router>
+        </main>
+      </div>
+    );
 }
 
 export default App;
